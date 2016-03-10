25f24bd550f1b0636f68e2589ab9bc7cbffbb296
neeraj9194 <kansalneeraj9194@gmail.com>
2016-03-10 23:11:00 +0530

    added fixed nav and horizontal scroll

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 50) {
+        $('.fixed-navbar').addClass('navbar-fixed-top');
+        $('.fixed-navbar').css("background-color", "rgba(1,165,136,1)");
+    } else {
+        $('.fixed-navbar').removeClass('navbar-fixed-top');
+        $('.fixed-navbar').css("background-color", "");
+    }
+});
